31a9f8e9631d917c1307d8541cc7c392dba8aa0e
Bora Guven <guvenbe@gmail.com>
2022-05-10 23:02:26 -0400

    creating child actors

--- src/main/java/com/vp/Main.java
+++ src/main/java/com/vp/Main.java
@@ -4,11 +4,14 @@ import akka.actor.typed.ActorSystem;
 
 public class Main {
     public static void main(String[] args) {
-        ActorSystem<String> actorSystem = ActorSystem.create(FirstSimpleBehavior.create(), "FirstActorSystem");
-        actorSystem.tell("Hello are you there?");
-        actorSystem.tell("This is the second message");
-        actorSystem.tell("say hello");
-        actorSystem.tell("who are you?");
-        actorSystem.tell("create a child");
+//        ActorSystem<String> actorSystem = ActorSystem.create(FirstSimpleBehavior.create(), "FirstActorSystem");
+//        actorSystem.tell("Hello are you there?");
+//        actorSystem.tell("This is the second message");
+//        actorSystem.tell("say hello");
+//        actorSystem.tell("who are you?");
+//        actorSystem.tell("create a child");
+
+        ActorSystem<String> bigPrimes = ActorSystem.create(ManagerBehavior.create(), "BigPrimes");
+        bigPrimes.tell("start");
     }
 }
