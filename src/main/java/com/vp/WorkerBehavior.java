f2a84c75319cbe265b3e2412c727b9f10eeb8c40
Bora Guven <guvenbe@gmail.com>
2022-05-10 23:31:53 -0400

    creating child actors

--- src/main/java/com/vp/WorkerBehavior.java
+++ src/main/java/com/vp/WorkerBehavior.java
@@ -1,29 +1,54 @@
 package com.vp;
 
+import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
 
+import java.io.Serializable;
 import java.math.BigInteger;
 import java.util.Random;
 
-public class WorkerBehavior extends AbstractBehavior<String> {
+public class WorkerBehavior extends AbstractBehavior<WorkerBehavior.Command> {
 
-    private WorkerBehavior(ActorContext context) {
+    public static class Command implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+        private String message;
+        private ActorRef<String> sender;
+
+        public Command(String message, ActorRef<String> sender) {
+            this.message = message;
+            this.sender = sender;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public ActorRef<String> getSender() {
+            return sender;
+        }
+
+       }
+
+    private WorkerBehavior(ActorContext<Command> context) {
         super(context);
     }
 
-    public static Behavior<String> create(){
+    public static Behavior<Command> create(){
         return Behaviors.setup(WorkerBehavior::new);
     }
     @Override
-    public Receive createReceive() {
+    public Receive<Command> createReceive() {
         return newReceiveBuilder()
-                .onMessageEquals("start", () ->{
-                    BigInteger bigInteger = new BigInteger(2000, new Random());
-                    System.out.println(bigInteger.nextProbablePrime());
+                .onAnyMessage(command ->{
+                    if(command.message.equals("start")){
+                        BigInteger bigInteger = new BigInteger(2000, new Random());
+                        System.out.println(bigInteger.nextProbablePrime());
+                    }
                     return this;
                 })
                 .build();
